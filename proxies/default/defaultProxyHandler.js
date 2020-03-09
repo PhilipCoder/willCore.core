87afd432917be0fa843feda757c36684b2fbc4b0
Philip Schoeman <philips@medicharge.co.za>
2020-03-09 22:17:51 +0200

    kjh

--- proxies/default/defaultProxyHandler.js
+++ proxies/default/defaultProxyHandler.js
@@ -4,21 +4,19 @@ const baseProxy = require("../base/baseProxy.js");
 class defaultProxyHandler extends assignableProxyHandler {
     constructor(parentProxy, parentProperty) {
         super();
-        this.setTraps.unshift(this.assignAssignable);
+        this.getTraps.unshift(this.getProxies);
         this.proxies = {};
-    }
-
-    assignAssignable(target, property, value, proxy) {
-        if (value instanceof baseProxy) {
-            this.proxies[property] = value;
-            return { status: true, value: value };
-        }
-        return { status: false };
     }
 
     getProxies(target, property, proxy) {
         if (property === "proxies") {
-            return { status: true, value: this.proxies };
+            let result = {};
+            for (let key in target){
+                if (!key.startsWith("_")){
+                    result[key] = target[key];
+                }
+            }
+            return { status: true, value: result };
         }
         return { status: false };
     }
--- test/default-proxy-test.js
+++ test/default-proxy-test.js
@@ -2,6 +2,7 @@ const assert = require('chai').assert;
 const willCoreProxy = require("../proxies/willCore/willCoreProxy.js");
 const defaultProxy = require("../proxies/default/defaultProxy.js");
 const willCoreModule = require("../moduleContainer/willCoreModules.js");
+const testProxy = require("../test/mocks/testAssignableProxy.js");
 
 describe('test-default-proxy-test', function () {
     before(function(){
@@ -17,7 +18,9 @@ describe('test-default-proxy-test', function () {
         let proxy = willCoreProxy.new();
         proxy.defaultValues;
         proxy.defaultValues.someDefaultValue.testProxy = "Hello world";
-        assert(proxy.defaultValues instanceof defaultProxy, "An defaultProxy was not returned.");
+        assert(proxy.defaultValues.someDefaultValue instanceof testProxy, "An testProxy was not returned.");
+        assert(Object.keys(proxy.defaultValues.proxies).length ===1 , "There should only be one key.");
+
     });
 })
 
